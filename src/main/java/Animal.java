62124160a48b4607aaf78c325cc054ca6bc66739
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-04-05 08:11:56 +0200

    Cleaning code. Added @Date

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,5 +1,7 @@
+import lombok.Data;
 import java.io.*;
 
+@Data
 public class Animal implements Serializable {
     static final long serialVersionUID = -8776502104304959804L;
     
@@ -18,7 +20,6 @@ public class Animal implements Serializable {
         setBarnID(-1);
         setVaccinated(false);
     }
-    
     public Animal(String species, int ageInMonths, String gender, int weightInKG, boolean vaccinated, int barnID) {
         setSpecies(species);
         setAgeInMonths(ageInMonths);
@@ -33,80 +34,8 @@ public class Animal implements Serializable {
         return getSpecies() + " | " + getGender() + " | waga " + weightInKG + " kg | "+ getAgeInMonths()/12 + " lat " + getAgeInMonths()%12 + " miesiecy | w stodole o ID: " + getBarnID() + " | " + vaccine;
     }
     
-    public String getSpecies() {
-        return species;
-    }
-    private void setSpecies(String species) {
-        this.species = species;
-    }
-    
-    public int getAgeInMonths() {
-        return ageInMonths;
-    }
-    private void setAgeInMonths(int ageInMonths) {
-        this.ageInMonths = ageInMonths;
-    }
-    
-    public String getGender() {
-        return gender;
-    }
-    private void setGender(String gender) {
-        this.gender = gender;
-    }
-    
-    public int getWeightInKG() {
-        return weightInKG;
-    }
-    public void setWeightInKG(int weightInKG) {
-        this.weightInKG = weightInKG;
-    }
-    
-    public int getBarnID() {
-        return barnID;
-    }
-    public void setBarnID(int barnID) {
-        this.barnID = barnID;
-    }
-    
-    public boolean isVaccinated() {
-        return isVaccinated;
-    }
-    public void setVaccinated(boolean vaccinated) {
-        isVaccinated = vaccinated;
-    }
-    
     @Override
     public String toString() {
         return "Animal{" + "species='" + species + '\'' + ", ageInMonths=" + ageInMonths + ", gender='" + gender + '\'' + ", weightInKG=" + weightInKG + ", barnID=" + barnID + ", isVaccinated=" + isVaccinated + '}';
-    }
-    
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        
-        Animal animal = (Animal) o;
-        
-        if (getAgeInMonths() != animal.getAgeInMonths())
-            return false;
-        if (weightInKG != animal.weightInKG)
-            return false;
-        if (getBarnID() != animal.getBarnID())
-            return false;
-        if (!getSpecies().equals(animal.getSpecies()))
-            return false;
-        return getGender().equals(animal.getGender());
-    }
-    
-    @Override
-    public int hashCode() {
-        int result = getSpecies().hashCode();
-        result = 31 * result + getAgeInMonths();
-        result = 31 * result + getGender().hashCode();
-        result = 31 * result + weightInKG;
-        result = 31 * result + getBarnID();
-        return result;
     }
 }
